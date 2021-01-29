ca507c20b0dd05631c1353bc8eaf0d4687b7d63e
ELIAZONE <eliaz_one@mail.ru>
2021-01-29 17:45:14 +0300

    main

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 
 // АНИМАЦИЯ БУРГЕРА //
-document.querySelector('.burger').addEventListener('click', function(){
+document.querySelector('.burger').addEventListener('click', function () {
     document.querySelector('.burger span').classList.toggle('active');
     })
+
 // ВЫЕЗЖАЕТ МЕНЮ //
 document.querySelector('.burger').addEventListener('click', function(){
     document.querySelector('.survive__column-menu').classList.toggle('animate');
+
+    if('.survive__column-menu.animate') {
+    document.querySelector('.survive__column-img').classList.toggle('active');
+    }
     })
