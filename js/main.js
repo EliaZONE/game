ef23c56a78e7eb0f275b3716a842662952f00396
ELIAZONE <eliaz_one@mail.ru>
2021-01-31 20:57:39 +0300

    main

--- js/main.js
+++ js/main.js
@@ -11,4 +11,29 @@ document.querySelector('.burger').addEventListener('click', function(){
     if('.survive__column-menu.animate') {
     document.querySelector('.survive__column-img').classList.toggle('active');
     }
-    })
+})
+
+// ПЛАВНАЯ НАВИГАЦИЯ ПО САЙТУ //
+
+const smoothLinks = document.querySelectorAll('a[href^="#"]');
+for (let smoothLink of smoothLinks) {
+    smoothLink.addEventListener('click', function (e) {
+        e.preventDefault();
+        const id = smoothLink.getAttribute('href');
+
+        document.querySelector(id).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+}; 
+
+// ПРОПАДАЕТ МЕНЮ ПРИ КЛИКЕ //
+
+document.querySelector('.survive__column-menu').addEventListener('click', function() {
+    document.querySelector('.survive__column-menu').classList.remove('animate');
+
+    if('.survive__column-menu.animate') {
+        document.querySelector('.survive__column-img').classList.remove('active');
+        }
+})
