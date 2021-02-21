fcda146505a66c12a7c74df0baae9176b06a7fee
ELIAZONE <eliaz_one@mail.ru>
2021-02-21 13:49:52 +0300

    main

--- js/main.js
+++ js/main.js
@@ -15,18 +15,60 @@ document.querySelector('.burger').addEventListener('click', function(){
 
 // ПЛАВНАЯ НАВИГАЦИЯ ПО САЙТУ //
 
-const smoothLinks = document.querySelectorAll(['a[href="#top"], a[href="#about"], a[href="#features"], a[href="#system_req"], a[href="#quotes"]']);
-for (let smoothLink of smoothLinks) {
-    smoothLink.addEventListener('click', function (e) {
-        e.preventDefault();
-        const id = smoothLink.getAttribute('href');
+(function()
+{
+let speed = 600;
+let moving_frequency = 1;
+let links = document.getElementsByTagName('a');
+let href;
+for(let i=0; i<links.length; i++)
+{   
+    href = (links[i].attributes.href === undefined) ? null : links[i].attributes.href.nodeValue.toString();
+    if(href !== null && href.length > 1 && href.substr(0, 1) == '#')
+    {
+        links[i].onclick = function()
+        {
+            let element;
+            let href = this.attributes.href.nodeValue.toString();
+            if(element = document.getElementById(href.substr(1)))
+            {
+                let hop_count = speed/moving_frequency;
+                let getScrollTopDocumentAtBegin = getScrollTopDocument();
+                let gap = (getScrollTopElement(element) - getScrollTopDocumentAtBegin) / hop_count;
 
-        document.querySelector(id).scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        });
-    });
-}; 
+                for(let i = 1; i <= hop_count; i++)
+                {
+                    (function()
+                    {
+                        let hop_top_position = gap*i;
+                        setTimeout(function(){  window.scrollTo(0, hop_top_position + getScrollTopDocumentAtBegin); }, moving_frequency*i);
+                    })();
+                }
+            }
+
+            return false;
+        };
+    }
+}
+
+let getScrollTopElement =  function (e)
+{
+    let top = 0;
+
+    while (e.offsetParent != undefined && e.offsetParent != null)
+    {
+        top += e.offsetTop + (e.clientTop != null ? e.clientTop : 0);
+        e = e.offsetParent;
+    }
+
+    return top;
+};
+
+let getScrollTopDocument = function()
+{
+    return document.documentElement.scrollTop + document.body.scrollTop;
+};
+})();
 
 // ПРОПАДАЕТ МЕНЮ ПРИ КЛИКЕ //
 
